407e8a55241ccbb7d03592cc87a4db6ea38c1e8a
imzhai <jue.yguel@gmail.com>
2019-08-20 12:01:52 +0200

    annotations

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class WelcomeController
+{
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function hello()
+    {
+        $name = 'Matthieu';
+
+        return new Response('Salut Boloss et '. $name);
+    }
+}
