8187ee16bada75ae5011ce1e0ce4101e7df463d2
imzhai <jue.yguel@gmail.com>
2019-08-20 14:29:23 +0200

    Install maker

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -2,23 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WelcomeController
+class WelcomeController extends AbstractController
 {
     /**
      * @Route("/hello", name="hello")
      */
-    public function hello(Request $request)
+    public function hello(Request $request) // Le typage Request active le component Request
     {
         $name = 'Matthieu';
-        
+
         // $request->get('a') équivaut à $_GET['a']
         dump($request);
 
-        return new Response('<body>Salut Boloss et '. $name . '</body>');
+        //return new Response('<body>Salut Boloss et '. $name . '</body>');
+        // On fait un rendu de la vue twig qui est située dans le dossier templates/
+        //Le controler passe la variable name à la vie grâce au second paramètre de render qui est un tableau
+        return $this->render('welcome/hello.html.twig', [
+            'name' => $name,
+        ]);
     }
 }
 
